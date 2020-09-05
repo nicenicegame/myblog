c3906aa5d789c155332ab9b99bfdc177d72cd378
Tatpol Samakpong <nicenicegame@gmail.com>
2020-09-05 17:26:37 +0700

    Completed basic blog app

--- blog/forms.py
+++ blog/forms.py
@@ -2,15 +2,27 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=20, min_length=4)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput, min_length=6)
+    username = forms.CharField(label='Username', max_length=20, min_length=4, widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }), min_length=6)
 
 
 class SignupForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=20, min_length=4)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput, min_length=6)
+    username = forms.CharField(label='Username', max_length=20, min_length=4, widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }), min_length=6)
 
 
 class PostForm(forms.Form):
-    post_title = forms.CharField(label='Post Title', max_length=100)
-    post_content = forms.CharField(label='Post Content', widget=forms.Textarea)
+    post_title = forms.CharField(label='Post Title', max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    post_content = forms.CharField(label='Post Content', widget=forms.Textarea(attrs={
+        'class': 'form-control'
+    }))
